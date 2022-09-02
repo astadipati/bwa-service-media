516c313a335aa00e7fd2dbd08e974d66ba2f916c
astadipati <asekbos@gmail.com>
2022-09-02 22:18:39 +0700

    find all

--- routes/media.js
+++ routes/media.js
@@ -6,6 +6,23 @@ const base64Img = require('base64-img');
 // model
 const {Media} = require('../models')
 
+router.get('/', async(req, res)=>{
+  const media = await Media.findAll({
+    // hanya panggil id dan image saja tanpa created & updated
+    attributes: ['id','image']
+  });
+  
+  const mappedMedia = media.map((m)=>{
+    m.image = `${req.get('host')}/${m.image}`;
+    return m
+  })
+
+  return res.json({
+    status:'success',
+    data: mappedMedia
+  });
+});
+
 router.post('/', (req, res) => {
   const image = req.body.image;
   if (!isBase64(image,{mimeRequired: true})) {
@@ -17,7 +34,7 @@ router.post('/', (req, res) => {
     }
     const filename = filepath.split('/').pop();
 
-    const media = await Media.create({image: `image/${filename}`});
+    const media = await Media.create({image: `images/${filename}`});
 
     return res.json({
       status: 'success',
